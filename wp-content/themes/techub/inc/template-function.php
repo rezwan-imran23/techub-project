72badf302c8a9a98ac30648f9669c524efc02c72
rezwan-imran23 <rezwan26imran@gmail.com>
2024-07-10 15:42:56 +0600

    second commit

--- wp-content/themes/techub/inc/template-function.php
+++ wp-content/themes/techub/inc/template-function.php
@@ -5,7 +5,7 @@
 function techub_header_logo(){
     $header_logo = get_theme_mod('header_logo',get_template_directory_uri().'/assets/img/logo/logo.png');
     ?>
-    <a href="<?php echo home_url('/') ?>"><img src="<?php echo $header_logo ?>" alt=""></a>
+    <a href="<?php echo home_url('/') ?>"><img src="<?php echo esc_url($header_logo); ?>" alt=""></a>
     <?php
 }
 
@@ -23,16 +23,16 @@ function techub_header_social(){
     ?>
 
     <?php if(!empty($header_facebook_url)):?>
-    <a href="<?php echo  $header_facebook_url ?>"><i class="fa-brands fa-facebook"></i></a>
+    <a href="<?php echo esc_url($header_facebook_url); ?>"><i class="fa-brands fa-facebook"></i></a>
     <?php endif; ?>
     <?php if(!empty($header_instagram_url)):?>
-    <a href="<?php echo  $header_instagram_url ?>"><i class="fa-brands fa-instagram"></i></a>
+    <a href="<?php echo esc_url($header_instagram_url); ?>"><i class="fa-brands fa-instagram"></i></a>
     <?php endif; ?>
     <?php if(!empty($header_twitter_url)):?>
-    <a href="<?php echo  $header_twitter_url ?>"><i class="fa-brands fa-twitter"></i></a>
+    <a href="<?php echo esc_url($header_twitter_url); ?>"><i class="fa-brands fa-twitter"></i></a>
     <?php endif; ?>
     <?php if(!empty( $header_pinterest_url)):?>
-    <a href="<?php echo  $header_pinterest_url ?>"><i class="fa-brands fa-pinterest"></i></a>
+    <a href="<?php echo esc_url($header_pinterest_url); ?>"><i class="fa-brands fa-pinterest"></i></a>
     <?php endif; ?>
 
 <?php
